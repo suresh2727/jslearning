a1dedd0018a6b8eb6e5659aef306760d59efc702
suresh mistry <52049092+suresh2727@users.noreply.github.com>
2023-12-27 19:03:20 +0000

    new file comparison js

---
+++ comparison.js
@@ -0,0 +1,15 @@
+// Comparison in javascript 
+
+// console.log("2" >= 2);
+// console.log("2" <= 2);
+// console.log("2" == 2);
+// console.log("2" != 3);
+// console.log("2" === 2);
+// console.log("2" > 2);
+// console.log("2" < 2);
+
+console.log(null == 0);
+console.log(null >= 2);
+console.log(null <= 0);
+
+console.log(typeof null);
