85f77e7d9c639592a200de7075fb497bff66daf9
suresh mistry <52049092+suresh2727@users.noreply.github.com>
2023-12-29 21:17:01 +0530

    Add files via upload

--- strings.js
+++ strings.js
@@ -15,4 +15,26 @@ console.log(namevalue.charAt(2));
 console.log(namevalue.indexOf("r"));
 
 console.log(namevalue.substring(0, 4));
-console.log(namevalue.);
+console.log(namevalue.slice(-2, 3));
+
+let newword = "   suresh   ";
+console.log(newword.trim()); // trim functions removes the whitespaces
+
+// replace
+
+let newpara = " I love to have good bikes and car ";
+console.log(newpara.replace("good", "great"));
+
+// split
+
+let splitexmple = "hi suresh how are you";
+let splitresult = splitexmple.split(" ");
+let charatex = splitexmple.charAt(8);
+
+console.log(typeof splitexmple);
+console.log(splitresult);
+console.log(typeof splitresult);
+console.log(charatex);
+
+
+
