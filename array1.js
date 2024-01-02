2efab5cf0901c67a76e55c8ee76d59d5c1558c37
suresh mistry <52049092+suresh2727@users.noreply.github.com>
2024-01-02 21:19:28 +0530

    Add files via upload

---
+++ array1.js
@@ -0,0 +1,17 @@
+//Arrays in javascript are use to store different datatypes in a order list and we can perform various task.
+
+
+let array = [1 ,3 ,5 ,9,7 ];
+
+console.log(array);
+console.log("\n");
+
+// Different way to enter array 
+
+let diffarray = new Array("suresh" , 5 , true)
+console.log(diffarray);
+console.log("\n");
+
+// Different methods in array 
+
+
